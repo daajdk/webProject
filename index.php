a50cc85e1219afc397f190bef485fe9883110afe
John Costantino <costan71@elvis.rowan.edu>
2014-04-14 00:35:43 -0400

    work

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php require_once 'include/header.php'; 
       require_once 'include/signin-header.php'; 
-      require_once 'login.php';
-      require_once 'createNew.php';
 ?>
 
 	<div class="main-container">
