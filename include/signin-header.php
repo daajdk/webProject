974c50c6688495203bc13d152ba8979df6d3133c
John Costantino <costan71@elvis.rowan.edu>
2014-04-13 20:47:43 -0400

    Updated js to autofill as well as css tweaks

--- include/signin-header.php
+++ include/signin-header.php
@@ -3,7 +3,7 @@
 
                 <div class="top-box">                 
                     <form class="signin" name="sign-in" action="#" method="post">
-                        <input required class="user-input" type="text" placeholder="email" <?php if (!empty($_POST['login_email'])) echo "value =" .  $_POST['login_email'] ?> name="login_email"><br>
+                        <input required class="user-input" type="text" placeholder="User Name" <?php if (!empty($_POST['login_email'])) echo "value =" .  $_POST['login_email'] ?> name="login_email"><br>
                         <input required class="user-input" type="password" placeholder="password"   name="login_password"><br>
                         <?php if(!empty($loginError)) echo $loginError . "<br>"?>
                         <input class="button" type="submit" value="Login">
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
                         <form class="signup" name="sign-up" action="#" method="post">
                             <input required class="user-input" type="text" placeholder="First Name"  name="firstname"><br>
                             <input required class="user-input" type="text" placeholder="Last Name"  name="lastname"><br>
-                            <input required class="user-input" type="text" placeholder="City"  name="city"><br>			    
+                            <input required class="user-input" id = "city" type="text" placeholder="City"  name="city"><br>			    
 			    <input required class="user-input" type="tel" placeholder="Phone Number"  name="phone"><br>
                             <input required class="user-input" type="text" placeholder="User Name"  name="customerID"><br>
                             <input required class="user-input" type="password" placeholder="Password"  name="password"><br>
@@ -24,5 +24,8 @@
 
             </div> <!-- .main -->
         </div> <!-- .main-container -->
+
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
+<script src="js/autoComplete.js"></script>
 
 <?php require 'include/footer.php'; ?>
