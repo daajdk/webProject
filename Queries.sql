e5cba19e7128f98122cf6ee7f3a885fd4451b35e
daajdk <mccarthyk1@students.rowan.edu>
2014-04-13 20:36:00 -0400

    updated

--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,4 @@
+/*Change customer info*/
 create procedure changeCustomerName(custID int, newfname varchar(25), newlname varchar(25))
        BEGIN
 	UPDATE customer
@@ -16,26 +17,57 @@ create procedure changeCustomerPhone(custID int, newphone varchar (10))
         SET phone = newphone
         WHERE customerID = custID
        END;
-
-create procedure changeSupplierName(suppID int, newname varchar (25))
+/*add new customer*/
+create procedure addNewCustomer(custID char(10), firstname varchar(25), lastname varchar(25), 
+       		 		ccity varchar(50), cphone char(10))
+       BEGIN
+	INSERT 
+	INTO customer(customerID, fname, lname, customercity, customerphone)
+	VALUES (custID, firstname, lastname, ccity, cphone 
+       END;
+/*delete customer*/
+create procedure deleteCustomer(custID char(10))
+       BEGIN
+	DELETE 
+	FROM customer
+	WHERE customerID = custID
+       END;
+	
+/*change supplier info*/
+create procedure changeSupplierName(suppID char(10), newname varchar (25))
        BEGIN
         UPDATE supplier
         SET sname = newname
         WHERE supplierID = suppID
        END;
-create procedure changeSupplierCity(suppID int, newcity varchar (50))
+create procedure changeSupplierCity(suppID char(10), newcity varchar (50))
        BEGIN
         UPDATE supplier
        	SET suppliercity = newcity
        	WHERE supplierID = suppID
        END;
-create procedure changeSupplierPhone(suppID int, newphone varchar (10))
+create procedure changeSupplierPhone(suppID char(10), newphone varchar (10))
        BEGIN
 	UPDATE supplier
        	SET supplierphone = newphone
        	WHERE supplierID = suppID
        END;
-
+/*add new supplier*/
+create procedure addNewSupplier(suppID char(10), suppname varchar(25)
+                                scity varchar(50), sphone char(10))
+       BEGIN
+        INSERT
+        INTO supplier(supplierID, sname, suppliercity, suplierphone)
+        VALUES (suppID, suppname,  scity, sphone
+       END;
+/*delete supplier*/
+create procedure deleteSupplier(suppID char(10))
+       BEGIN
+        DELETE
+        FROM supplier
+        WHERE supplierID = suppID
+       END;
+/*change product info*/
 create procedure changeProductName(sku char(13), newname varchar(25))
        BEGIN
 	UPDATE product
@@ -55,6 +87,7 @@ create procedure changeProductType(sku char(13), newtype varchar(25))
         WHERE upc = sku
        END;
 
+/*change product pricing*/
 create procedure changeProductPrice(suppID char(10), sku char(13), newprice decimal(5,2))
        BEGIN
 	UPDATE sells
@@ -67,10 +100,13 @@ create procedure changeProductShippingCost(suppID char(10), sku char(13), newpri
 	SET shipcost = newprice
 	WHERE sells.supplierID = suppID and sells.upc = sku
        END;
-
+/*1.E Ablility to view Inventory*/
 create procedure showInventory(suppID char(10))
        BEGIN
 	SELECT supplierID, sname, pname, ptype, category, inventory, price 
 	FROM (sells join product using(upc)) join supplier using (supplierID)
 	WHERE supplierID = suppID
-       END;		 
+       END;
+
+/*skipping 1.F*/
+		 
